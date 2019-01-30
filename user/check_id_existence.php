27bf5200842268ca6f439c383dc7357e89480871
ForSeason <ysy604822@live.com>
2019-01-31 00:56:14 +0800

    刚刚那个接口空格没对齐，强迫症忍不了再上传一次

--- user/check_id_existence.php
+++ user/check_id_existence.php
@@ -1,10 +1,10 @@
 <?php
     require_once('../method/pdo_handler.php');
     if (isset($_POST['id'])) {
-        $table = 'user';
+        $table  = 'user';
         $params = array('id');
         $values = array($_POST['id']);
-        $link = new pdo_handler();
+        $link   = new pdo_handler();
         echo $link->select($table, null, $params, $values)->rowCount();
     } else {
         echo 'go away!';
