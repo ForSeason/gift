c72b425c231d442d665ec7b0a899f6fba10da10e
ForSeason <ysy604822@live.com>
2019-02-11 15:23:14 +0800

    给获取事件列表的接口加上了种类的控制

--- user/get_event_list.php
+++ user/get_event_list.php
@@ -1,13 +1,14 @@
 <?php
     // require_once('../method/user_handler.php');
     require_once('../method/chat_handler.php');
-    if (isset($_POST['start']) and isset($_POST['step'])) {
+    if (isset($_POST['start']) and isset($_POST['step']) and isset($_POST['type'])) {
         $link  = new pdo_handler();
         $start = (int)$_POST['start'];
         $step  = (int)$_POST['step'];
+        $type  = (int)$_POST['type'];
         $table = 'events';
-        $tpl   = 'ORDER BY eid DESC LIMIT %s, %s';
-        $limit = sprintf($tpl, (string)$start, (string)$step);
+        $tpl   = 'type=%s ORDER BY eid DESC LIMIT %s, %s';
+        $limit = sprintf($tpl, (string)$type, (string)$start, (string)$step);
         $arr   = $link->select_sp($table, array('eid'), $limit)->fetchAll(PDO::FETCH_ASSOC);
         $res   = array();
         foreach ($arr as $key => $value) {
