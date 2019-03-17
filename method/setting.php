8ab3a8a59a67ca96cb0c18a08ed2f09fb744bd5c
ForSeason <44540777+ForSeason@users.noreply.github.com>
2019-03-17 22:56:07 +0800

    Delete setting.php

--- method/setting.php
+++
@@ -1,9 +0,0 @@
-<?php
-    header('Content-Type:text/html;charset=utf-8');
-    header("Access-Control-Allow-Origin:http://localhost:8080");
-    header("Access-Control-Allow-Credentials:true");
-    date_default_timezone_set("Asia/Shanghai");
-    define('DATABASE_NAME','mysql:host=localhost;dbname=gift');
-    define('DATABASE_ID','root');
-    define('DATABASE_PW','');
-?>
